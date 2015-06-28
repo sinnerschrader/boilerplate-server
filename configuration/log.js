296c70ab69e90cdfd57a154d26c0c88b25cf6ebf
Mario Nebl <mario.nebl@sinnerschrader.com>
2015-06-28 21:40:11 +0200

    Disable file logging by default

--- configuration/log.js
+++ configuration/log.js
@@ -5,17 +5,12 @@ Object.defineProperty(exports, '__esModule', {
 });
 exports['default'] = {
 	'level': process.env.BOILERPLATESERVER_LOG_LEVEL || process.env.BOILERPLATE_LOG_LEVEL || process.env.NODE_LOG_LEVEL || process.env.LOG_LEVEL || 'silly',
-	'transports': ['console', 'file'],
+	'transports': ['console'],
 	'options': {
 		'console': {
 			'colorize': false,
 			'timestamp': false,
 			'showLevel': false
-		},
-		'file': {
-			'colorize': false,
-			'timestamp': true,
-			'showLevel': true
 		}
 	}
 };
